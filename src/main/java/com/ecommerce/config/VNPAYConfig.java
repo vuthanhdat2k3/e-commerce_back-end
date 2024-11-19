663d32aeb8f5cfbf3edf74774b0167d3a8aa2bb8
vuthanhdat2k3 <vuthanhdat19052k3@gmail.com>
2024-11-19 18:51:40 +0700

    update

--- src/main/java/com/ecommerce/config/VNPAYConfig.java
+++ src/main/java/com/ecommerce/config/VNPAYConfig.java
@@ -13,41 +13,60 @@ import java.util.TimeZone;
 
 @Configuration
 public class VNPAYConfig {
+
     @Getter
     @Value("${payment.vnPay.url}")
     private String vnp_PayUrl;
+
+    @Getter
     @Value("${payment.vnPay.returnUrl}")
     private String vnp_ReturnUrl;
+
+    @Getter
     @Value("${payment.vnPay.tmnCode}")
-    private String vnp_TmnCode ;
+    private String vnp_TmnCode;
+
     @Getter
     @Value("${payment.vnPay.secretKey}")
     private String secretKey;
+
+    @Getter
     @Value("${payment.vnPay.version}")
     private String vnp_Version;
+
+    @Getter
     @Value("${payment.vnPay.command}")
     private String vnp_Command;
+
+    @Getter
     @Value("${payment.vnPay.orderType}")
     private String orderType;
 
+    private static final String TIMEZONE = "Asia/Ho_Chi_Minh";
+    private static final String CURRENCY_CODE = "VND";
+    private static final String LOCALE = "vn";
+
     public Map<String, String> getVNPayConfig() {
         Map<String, String> vnpParamsMap = new HashMap<>();
-        vnpParamsMap.put("vnp_Version", this.vnp_Version);
-        vnpParamsMap.put("vnp_Command", this.vnp_Command);
-        vnpParamsMap.put("vnp_TmnCode", this.vnp_TmnCode);
-        vnpParamsMap.put("vnp_CurrCode", "VND");
-        vnpParamsMap.put("vnp_TxnRef",  VNPayUtil.getRandomNumber(8));
-        vnpParamsMap.put("vnp_OrderInfo", "Thanh toan don hang:" +  VNPayUtil.getRandomNumber(8));
-        vnpParamsMap.put("vnp_OrderType", this.orderType);
-        vnpParamsMap.put("vnp_Locale", "vn");
-        vnpParamsMap.put("vnp_ReturnUrl", this.vnp_ReturnUrl);
-        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        vnpParamsMap.put("vnp_Version", vnp_Version);
+        vnpParamsMap.put("vnp_Command", vnp_Command);
+        vnpParamsMap.put("vnp_TmnCode", vnp_TmnCode);
+        vnpParamsMap.put("vnp_CurrCode", CURRENCY_CODE);
+        vnpParamsMap.put("vnp_TxnRef", VNPayUtil.getRandomNumber(8));
+        vnpParamsMap.put("vnp_OrderInfo", "Thanh toan don hang: " + VNPayUtil.getRandomNumber(8));
+        vnpParamsMap.put("vnp_OrderType", orderType);
+        vnpParamsMap.put("vnp_Locale", LOCALE);
+        vnpParamsMap.put("vnp_ReturnUrl", vnp_ReturnUrl);
+
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone(TIMEZONE));
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
         String vnpCreateDate = formatter.format(calendar.getTime());
         vnpParamsMap.put("vnp_CreateDate", vnpCreateDate);
-        calendar.add(Calendar.MINUTE, 60);
+
+        calendar.add(Calendar.MINUTE, 60); // Thêm 60 phút cho vnp_ExpireDate
         String vnp_ExpireDate = formatter.format(calendar.getTime());
         vnpParamsMap.put("vnp_ExpireDate", vnp_ExpireDate);
+
         return vnpParamsMap;
     }
 }
